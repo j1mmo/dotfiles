b41082de931f90bc82079c69ad66a12e65909c33
James Sherlock-Shaw <jsherlockshaw@gmail.com>
2020-09-04 16:25:00 +0100

    small changes, removed bell sound

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,3 +11,18 @@
 (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/_init.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 173 :width normal))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
