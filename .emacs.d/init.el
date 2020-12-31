bc32492a616af00df7a81e28ae042b10f9f0a669
James Sherlock-Shaw <jsherlockshaw@gmail.com>
2020-12-31 15:26:20 +0000

    delete somethings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,11 +18,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+    (haskell-mode auto-complete magit centered-window modus-operandi-theme modus-vivendi-theme try counsel ace-window which-key org-bullets use-package)))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 173 :width normal))))
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 173 :width normal)))))
