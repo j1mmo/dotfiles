db00269fc6f29c83abbe97892bd97d4b60217b93
James Sherlock-Shaw <jsherlockshaw@gmail.com>
2021-04-17 17:23:04 +0100

    added some shiiiet

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,14 +17,46 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-enabled-themes '(zenburn almost-mono-black use-package))
+ '(custom-safe-themes
+   '("8f5b54bf6a36fe1c138219960dd324aad8ab1f62f543bed73ef5ad60956e36ae" "e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" "dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "a5d04a184d259f875e3aedbb6dbbe8cba82885d66cd3cf9482a5969f44f606c0" default))
+ '(fci-rule-color "#383838")
  '(menu-bar-mode nil)
+ '(nrepl-message-colors
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
  '(package-selected-packages
-   (quote
-    (haskell-mode auto-complete magit centered-window modus-operandi-theme modus-vivendi-theme try counsel ace-window which-key org-bullets use-package)))
- '(tool-bar-mode nil))
+   '(cpputils-cmake haskell-mode auto-complete magit centered-window modus-operandi-theme modus-vivendi-theme try counsel ace-window which-key org-bullets use-package))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
+ '(tool-bar-mode nil)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   '((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3")))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 173 :width normal)))))
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 173 :width normal))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 echo "Installing dotfiles..."
-
+echo "Removing existings files"
+rm -rf ~/.emacs.d
+rm -rf ~/.vimrc
 echo "Installing emacs"
-
 ln -s ~/.dotfiles/.emacs.d ~/.emacs.d
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 echo "Install complete"
